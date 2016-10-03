304d4b5880742be4bb1f6f17123dff78305a8774
Josh Green <greenjm@rose-hulman.edu>
2016-10-03 12:06:21 -0400

    Finished CssPropertyBasics exercises

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ class MainHandler(webapp2.RequestHandler):
         css_track.append(("HTML Basics - Tag Practice", True, "/static/HtmlBasics/tagPractice", 10))  # These links won't work yet.  That's ok.
         css_track.append(("CSS Selectors - Primary CSS Selectors", True, "/static/CssSelectors/primary_css_selector_exercises", 3))
         css_track.append(("CSS Selectors - Advanced CSS Selectors", True, "/static/CssSelectors/advanced_selector_exercises", 3))
+        css_track.append(("CSS Properties - Font Exercises", True, "/static/CssPropertyBasics/font_exercises", 3))
+        css_track.append(("CSS Properties - Background Exercises", True, "/static/CssPropertyBasics/background_and_border_exercises", 4))
 
         js_track = []
 
---
+++ static/CssPropertyBasics/README.md
@@ -0,0 +1,4 @@
+<h2>CSS Property Basics</h2>
+<p>
+  Practicing with some of the day-to-day values used in CSS.
+</p>
